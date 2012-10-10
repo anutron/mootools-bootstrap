0cef3011fc27bbc3aa2f5a81201299952a518f43
Aaron Newton <git@clientcide.com>
2012-10-10 16:05:51 -0700

    Increasing support for various HTML configurations for bootstrap form validation.

--- Source/Behaviors/Behavior.BS.FormValidator.js
+++ Source/Behaviors/Behavior.BS.FormValidator.js
@@ -18,56 +18,72 @@ provides: [Behavior.BS.FormValidator]
 ...
 */
 
-Behavior.addGlobalPlugin("FormValidator", "BS.FormValidator", {
-	setup: function(element, api, instance){
-		var original = {
-			showError: instance.options.showError,
-			hideError: instance.options.hideError
-		};
-		instance.setOptions({
-			showError: function(){},
-			hideError: function(){}
-		});
-		instance.warningPrefix = '';
-		instance.errorPrefix = '';
-		instance.addEvents({
-			showAdvice: function(field, advice, className){
-				var cls = field.hasClass('warning') || field.hasClass('warn-' + className) ? 'warn' : 'error',
-				    inputParent = field.getParent('.controls'),
-				    clearfixParent = inputParent ? inputParent.getParent('.control-group') : null;
-				if (!inputParent || !clearfixParent){
-					original.showError(advice);
-				} else {
-					field.addClass(cls);
-					var help = inputParent.getElement('div.advice');
-					if (!help){
-						inputParent.getElements('span.help-inline').setStyle('display', 'none');
-						help = new Element('span.help-inline.advice.auto-created', {
-							html: (field.hasClass('warning') ? 'Suggestion: ' : '') + advice.get('html')
-						}).hide().inject(field, 'after');
+(function(){
+
+	var getFieldDetails = function(field, advice, className){
+		var cls = field.hasClass('warning') || field.hasClass('warn-' + className) ? 'warn' : 'error',
+		    inputParent = field.getParent('.controls, .control-group');
+		var clearfixParent;
+		if (inputParent){
+			if (inputParent.hasClass('control-group')) clearfixParent = inputParent;
+			else clearfixParent = inputParent.getParent('.control-group');
+		}
+
+		return {
+			cls: cls,
+			inputParent: inputParent,
+			clearfixParent: clearfixParent
+		}
+	};
+
+	Behavior.addGlobalPlugin("FormValidator", "BS.FormValidator", {
+		setup: function(element, api, instance){
+			var original = {
+				showError: instance.options.showError,
+				hideError: instance.options.hideError
+			};
+			instance.setOptions({
+				showError: function(){},
+				hideError: function(){}
+			});
+			instance.warningPrefix = '';
+			instance.errorPrefix = '';
+			instance.addEvents({
+				showAdvice: function(field, advice, className){
+					var fieldDetails = getFieldDetails(field, advice, className);
+					if (!fieldDetails.inputParent || !fieldDetails.clearfixParent){
+						original.showError(advice);
+					} else {
+						field.addClass(fieldDetails.cls);
+						var help = fieldDetails.inputParent.getElement('div.advice');
+						if (!help){
+							fieldDetails.inputParent.getElements('span.help-inline').setStyle('display', 'none');
+							help = new Element('span.help-inline.advice.auto-created', {
+								html: (field.hasClass('warning') ? 'Suggestion: ' : '') + advice.get('html')
+							}).hide().inject(field, 'after');
+						}
+						help.set('html', (field.hasClass('warning') ? 'Suggestion: ' : '') + advice.get('html')).reveal();
+						help.removeClass('hide');
+						help.set('title', advice.get('html'));
+						fieldDetails.clearfixParent.addClass(fieldDetails.cls);
 					}
-					help.set('html', (field.hasClass('warning') ? 'Suggestion: ' : '') + advice.get('html')).reveal();
-					help.removeClass('hide');
-					help.set('title', advice.get('html'));
-					clearfixParent.addClass(cls);
+				},
+				hideAdvice: function(field, advice, className){
+					var fieldDetails = getFieldDetails(field, advice, className);
+					if (!fieldDetails.inputParent || !fieldDetails.clearfixParent){
+						original.hideError(advice);
+					} else {
+						field.removeClass(fieldDetails.cls);
+						var help = fieldDetails.inputParent.getElement('.advice');
+						fieldDetails.inputParent.getElements('.help-inline, .help-block').dissolve().getLast().get('reveal').chain(function(){
+							if (help.hasClass('auto-created')) help.destroy();
+							else help.set('html', '');
+						});
+						fieldDetails.clearfixParent.removeClass(fieldDetails.cls);
+					}
 				}
-			},
-			hideAdvice: function(field, advice, className){
-				var cls = field.hasClass('warnOnly') || field.hasClass('warn-' + className) ? 'warn' : 'error',
-				    inputParent = field.getParent('.controls'),
-				    clearfixParent = inputParent ? inputParent.getParent('.control-group') : null;
-				if (!inputParent || !clearfixParent){
-					original.hideError(advice);
-				} else {
-					field.removeClass(cls);
-					var help = inputParent.getElement('.advice');
-					inputParent.getElements('.help-inline, .help-block').dissolve().getLast().get('reveal').chain(function(){
-						if (help.hasClass('auto-created')) help.destroy();
-						else help.set('html', '');
-					});
-					clearfixParent.removeClass(cls);
-				}
-			}
-		});
-	}
-});
+			});
+		}
+	});
+
+})();
