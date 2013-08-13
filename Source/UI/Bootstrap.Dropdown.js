775e9e5d5e468dad2bbca106ddf157547c36e5d1
Aaron Newton <git@clientcide.com>
2013-08-13 15:10:30 -0700

    indentation fix.

--- Source/UI/Bootstrap.Dropdown.js
+++ Source/UI/Bootstrap.Dropdown.js
@@ -63,13 +63,13 @@ Bootstrap.Dropdown = new Class({
 		var open = el.getParent('.open');
 		if (!el.match(this.options.ignore) || !open) this.hideAll();
 		if (this.element.contains(el)) {
-            var parent = null;
-            if (el.match('[data-toggle="dropdown"]') || el.getParent('[data-toggle="dropdown"] !')) {
-                parent = el.getParent('.dropdown, .btn-group');
-            }
-            // backwards compatibility
-            if (!parent) parent = el.match('.dropdown-toggle') ? el.getParent() : el.getParent('.dropdown-toggle !');
-			if (parent) {
+			var parent;
+			if (el.match('[data-toggle="dropdown"]') || el.getParent('[data-toggle="dropdown"] !')){
+				parent = el.getParent('.dropdown, .btn-group');
+			}
+ 			// backwards compatibility
+			if (!parent) parent = el.match('.dropdown-toggle') ? el.getParent() : el.getParent('.dropdown-toggle !');
+			if (parent){
 				e.preventDefault();
 				if (!open) this.show(parent);
 			}
