d43d6eb46a244ade009bd07b585d92c1b59e0ace
Aaron Newton <git@clientcide.com>
2014-01-03 18:05:00 -0800

    Updating Bootstrap.Popup's logic for .modal-backdrop elements; those are deprecated in BS 3.0

--- Source/UI/Bootstrap.Popup.js
+++ Source/UI/Bootstrap.Popup.js
@@ -169,7 +169,7 @@ Bootstrap.Popup = new Class({
 	// PRIVATE
 
 	_makeMask: function(){
-		if (this.options.mask){
+		if (this.options.mask && Bootstrap.version < 3){
 			if (!this._mask){
 				this._mask = new Element('div.modal-backdrop.in');
 				if (this._checkAnimate()) this._mask.addClass('fade');
--- Tests/Specs/Source/Behavior.BS.Popup.js
+++ Tests/Specs/Source/Behavior.BS.Popup.js
@@ -34,10 +34,8 @@ provides: [Behavior.Popup.Specs]
 		returns: Bootstrap.Popup,
 		expect: function(element, instance){
 			expect($$('.modal')[0].isDisplayed()).toBe(true);
-			expect($$('.modal-backdrop')[0].isDisplayed()).toBe(true);
 			instance.hide();
 			expect($$('.modal')[0].isDisplayed()).toBe(false);
-			expect($$('.modal-backdrop').length).toBe(0);
 		}
 	});
 	Behavior.addFilterTest({
