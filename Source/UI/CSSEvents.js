a2c9eb952bc4a6a133a4c46433473d3eb9e9881f
Aaron Newton <git@clientcide.com>
2014-01-08 17:18:38 -0800

    Whitespace police

--- Source/UI/CSSEvents.js
+++ Source/UI/CSSEvents.js
@@ -15,25 +15,25 @@ provides: CSSEvents
 
 Browser.Features.getCSSTransition = function(){
 	Browser.Features.transitionEnd = (function(){
-    var el = document.createElement('tmp');
+		var el = document.createElement('tmp');
 
-    var transEndEventNames = {
-      'WebkitTransition' : 'webkitTransitionEnd'
-    , 'MozTransition'    : 'transitionend'
-    , 'OTransition'      : 'oTransitionEnd otransitionend'
-    , 'transition'       : 'transitionend'
-    };
+		var transEndEventNames = {
+			'WebkitTransition' : 'webkitTransitionEnd'
+		, 'MozTransition'    : 'transitionend'
+		, 'OTransition'      : 'oTransitionEnd otransitionend'
+		, 'transition'       : 'transitionend'
+		};
 
-    for (var name in transEndEventNames) {
-      if (el.style[name] !== undefined) {
-        return transEndEventNames[name];
-      }
-    }
-  })();
-  Browser.Features.cssTransition = !!Browser.Features.transitionEnd;
+		for (var name in transEndEventNames) {
+			if (el.style[name] !== undefined) {
+				return transEndEventNames[name];
+			}
+		}
+	})();
+	Browser.Features.cssTransition = !!Browser.Features.transitionEnd;
 
 	Browser.Features.getCSSTransition = Function.from(Browser.Features.transitionEnd);
-  return Browser.Features.transitionEnd;
+	return Browser.Features.transitionEnd;
 };
 
 window.addEvent("domready", Browser.Features.getCSSTransition);
